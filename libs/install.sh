d2a7208cb6edab693872b42dc5250c21cbd706ad
hoon <lovedeer118@gmail.com>
2017-06-29 12:05:37 +0900

    aws build.sh 작설ㅇ aws tomcat server.xml

--- libs/install.sh
+++ libs/install.sh
@@ -1,2 +1,3 @@
 #! /bin/bash
-mvn install:install-file -DgroupId=com.microsoft.sqlserver -DartifactId=sqljdbc4 -Dversion=4.0 -Dpackaging=jar -Dfile=libs/sqljdbc4-4.0.jar
+mvn install:install-file -DgroupId=com.microsoft.sqlserver -DartifactId=sqljdbc4 -Dversion=4.0 -Dpackaging=jar -Dfile=sqljdbc4-4.0.jar
+mvn install:install-file -DgroupId=com.oracle -DartifactId=mysql-connector -Dversion=5.1.40 -Dpackaging=jar -Dfile=mysql-connector-java-5.1.40-bin.jar 

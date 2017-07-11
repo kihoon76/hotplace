5719a04131eaba1f00ffe790e761b841203bbc4e
khnam <lovedeer118@gmail.com>
2017-07-11 13:48:34 +0900

    shell modify

--- libs/install.sh
+++ libs/install.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 mvn install:install-file -DgroupId=com.microsoft.sqlserver -DartifactId=sqljdbc4 -Dversion=4.0 -Dpackaging=jar -Dfile=sqljdbc4-4.0.jar
-mvn install:install-file -DgroupId=com.oracle -DartifactId=mysql-connector -Dversion=5.1.40 -Dpackaging=jar -Dfile=mysql-connector-java-5.1.40-bin.jar 
+#mvn install:install-file -DgroupId=com.oracle -DartifactId=mysql-connector -Dversion=5.1.40 -Dpackaging=jar -Dfile=mysql-connector-java-5.1.40-bin.jar 

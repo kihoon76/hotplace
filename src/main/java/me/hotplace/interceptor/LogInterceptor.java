3ffc8fda7e1929287da22f78df0bc6005572bae5
Your Name <you@example.com>
2017-12-18 13:23:47 +0900

    search

--- src/main/java/me/hotplace/interceptor/LogInterceptor.java
+++ src/main/java/me/hotplace/interceptor/LogInterceptor.java
@@ -27,20 +27,25 @@ public class LogInterceptor extends HandlerInterceptorAdapter {
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
 		
-		LogVO log = new LogVO();
+		String ip = HttpHeaderUtil.getClientIP(request);
 		
-		log.setIp(HttpHeaderUtil.getClientIP(request));
-		log.setId(getId());
-		log.setReferer(request.getHeader("referer"));
-		log.setUrl(request.getRequestURL().toString());
-		
-		String param = request.getQueryString();
-		if(param != null) {
-			log.setParameter(URLDecoder.decode(param, "UTF-8"));
+		if(!ip.startsWith("192.")) {
+			LogVO log = new LogVO();
+			
+			log.setIp(ip);
+			log.setId(getId());
+			log.setReferer(request.getHeader("referer"));
+			log.setUrl(request.getRequestURL().toString());
+			
+			String param = request.getQueryString();
+			if(param != null) {
+				log.setParameter(URLDecoder.decode(param, "UTF-8"));
+			}
+			
+			
+			producer.sendMessage(gson.toJson(log));
 		}
 		
-		
-		producer.sendMessage(gson.toJson(log));
 		return true;
 	}
 	
